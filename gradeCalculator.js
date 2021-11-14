86a4fd4906216f8520be3444331bc77e60fc690f
ferdif <ferdifh82@gmail.com>
2021-11-14 15:42:04 +0700

    add new code without function

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -1,73 +1,63 @@
-//  * Task 2
+//* Case 2
+
+let mtk = 80;
+let bahasaIndonesia = 90;
+let bahasaInggris = 89;
+let ipa = `69`;
+
+const totalScore = mtk + bahasaIndonesia + bahasaInggris + ipa;
+const averageScore = totalScore / 4;
+
+// validate total score
+if (typeof totalScore !== `number`) {
+    console.log(`Please enter score with number!`);
+} else if (Number.isNaN(totalScore)) {
+    console.log(`Score is not complete. Please enter all score!`);
+} else {
+    console.log(`Rata-rata = ${averageScore}`);
+}
+
+// validate type of grade
+if (averageScore >= 90 && averageScore <= 100) {
+    console.log(`Grade = A`);
+} else if (averageScore >= 80 && averageScore <= 89) {
+    console.log(`Grade = B`);
+} else if (averageScore >= 70 && averageScore <= 79) {
+    console.log(`Grade = C`);
+} else if (averageScore >= 60 && averageScore <= 69) {
+    console.log(`Grade = D`);
+} else if (averageScore >= 0 && averageScore <= 59) {
+    console.log(`Grade = E`);
+} else {
+    console.log(`I'm sorry you couldn't make it`);
+}
+
+
+//* Case 2: Grade in function
 
 gradeCalculator = (bahasa, english, maths, science) => {
     const subjects = bahasa + english + maths + science;
-    const gradeMedian = (subjects / 4);
+    const averageScore = (subjects / 4);
     
-    if (typeof subjects !== `number` || Number.isNaN(subjects)) {
-        return `You must enter all subjects with number!`;
+    if (typeof subjects !== `number`) {
+        return `Please enter your score with number!`;
+    } else if (Number.isNaN(subjects)) {
+        return `You must enter all subjects!`
     } else {
-        if (gradeMedian >= 90 && gradeMedian <= 100) {
-            return `Rata-rata = ${gradeMedian}\nGrade = A`;
-        } else if (gradeMedian >= 80 && gradeMedian <= 89) {
-            return `Rata-rata = ${gradeMedian}\nGrade = B`;
-        } else if (gradeMedian >= 70 && gradeMedian <= 79) {
-            return `Rata-rata = ${gradeMedian}\nGrade = C`;
-        } else if (gradeMedian >= 60 && gradeMedian <= 69) {
-            return `Rata-rata = ${gradeMedian}\nGrade = D`;
-        } else if (gradeMedian >= 0 && gradeMedian <= 59){
-            return `Rata-rata = ${gradeMedian}\nGrade = E`;
+        if (averageScore >= 90 && averageScore <= 100) {
+            return (`Rata-rata = ${averageScore} \nGrade = A`);
+        } else if (averageScore >= 80 && averageScore <= 89) {
+            return (`Rata-rata = ${averageScore} \nGrade = B`);
+        } else if (averageScore >= 70 && averageScore <= 79) {
+            return (`Rata-rata = ${averageScore} \nGrade = C`);
+        } else if (averageScore >= 60 && averageScore <= 69) {
+            return (`Rata-rata = ${averageScore} \nGrade = D`);
+        } else if (averageScore >= 0 && averageScore <= 59) {
+            return (`Rata-rata = ${averageScore} \nGrade = E`);
         } else {
-            return `You're not very bright, are you?`;
+            return (`You are not very bright, are you?`);
         }
     }
 }
 
-console.log(gradeCalculator(80, 84, 77, 96));
-
-// * example
-
-// const hitungNilai = (a, b, c, d) => {
-//     let nilai = a + b + c + d;
-//     let nilaiRata = parseInt(nilai / 4);
-
-//     // validasi jumlah nilai yang dimasukkan
-//     if (typeof nilai !== `number` || Number.isNaN(nilai)) {
-//         return `Please enter grade with number!`;
-//     } else {
-//         if (nilaiRata >= 90 && nilaiRata <= 100) {
-//             return `Rata-rata = ${nilaiRata}\nGrade = A`;
-//         } else if (nilaiRata >= 80 && nilaiRata <= 89) {
-//             return `Rata-rata = ${nilaiRata}\nGrade = B`;
-//         } else if (nilaiRata >= 70 && nilaiRata <= 79) {
-//             return `Rata-rata = ${nilaiRata}\nGrade = C`;
-//         } else if (nilaiRata >= 60 && nilaiRata <= 69) {
-//             return `Rata-rata = ${nilaiRata}\nGrade = D`;
-//         } else if (nilaiRata >= 0 && nilaiRata <= 59){
-//             return `Rata-rata = ${nilaiRata}\nGrade = E`;
-//         } else {
-//             return `You're not very bright, are you?`;
-//         }
-//     }
-// };
-
-
-// console.log(hitungNilai(70, 70, true, 86));
-
-// grade
-
-// function gradeMaker (params) {
-//     if (params >= 90 && params <= 100) {
-//         return `Grade = A`
-//     } else if (params >= 80 && params <= 89) {
-//         return `Grade = B`
-//     } else if (params >= 70 && params <= 79) {
-//         return `Grade = C`
-//     } else if (params >= 60 && params <= 69) {
-//         return `Grade = D`
-//     } else {
-//         return `Grade = E`
-//     }
-// }
-
-// console.log(gradeMaker(59))
+console.log(gradeCalculator(80, 84, 96, 77));
