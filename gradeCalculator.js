66e878cec312941abf1a299cecbcb3ecb1fb1a01
ferdif <ferdifh82@gmail.com>
2021-11-15 06:28:20 +0700

    refactoring code

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -3,33 +3,33 @@
 let mtk = 80;
 let bahasaIndonesia = 90;
 let bahasaInggris = 89;
-let ipa = `69`;
+let ipa = 69;
 
 const totalScore = mtk + bahasaIndonesia + bahasaInggris + ipa;
 const averageScore = totalScore / 4;
 
 // validate total score
-if (typeof totalScore !== `number`) {
-    console.log(`Please enter score with number!`);
+if (typeof totalScore !== 'number') {
+    console.log('Please insert your score with number!');
 } else if (Number.isNaN(totalScore)) {
-    console.log(`Score is not complete. Please enter all score!`);
+    console.log('Score is not complete. Please insert score from all subjects!');
 } else {
     console.log(`Rata-rata = ${averageScore}`);
 }
 
 // validate type of grade
 if (averageScore >= 90 && averageScore <= 100) {
-    console.log(`Grade = A`);
+    console.log('Grade = A');
 } else if (averageScore >= 80 && averageScore <= 89) {
-    console.log(`Grade = B`);
+    console.log('Grade = B');
 } else if (averageScore >= 70 && averageScore <= 79) {
-    console.log(`Grade = C`);
+    console.log('Grade = C');
 } else if (averageScore >= 60 && averageScore <= 69) {
-    console.log(`Grade = D`);
+    console.log('Grade = D');
 } else if (averageScore >= 0 && averageScore <= 59) {
-    console.log(`Grade = E`);
+    console.log('Grade = E');
 } else {
-    console.log(`I'm sorry you couldn't make it`);
+    console.log('I am sorry you couldn\'t make it');
 }
 
 
@@ -39,10 +39,10 @@ gradeCalculator = (bahasa, english, maths, science) => {
     const subjects = bahasa + english + maths + science;
     const averageScore = (subjects / 4);
     
-    if (typeof subjects !== `number`) {
-        return `Please enter your score with number!`;
+    if (typeof subjects !== 'number') {
+        return 'Please insert your score with number!';
     } else if (Number.isNaN(subjects)) {
-        return `You must enter all subjects!`
+        return 'Score is not complete. You must insert all subjects!';
     } else {
         if (averageScore >= 90 && averageScore <= 100) {
             return (`Rata-rata = ${averageScore} \nGrade = A`);
@@ -55,9 +55,9 @@ gradeCalculator = (bahasa, english, maths, science) => {
         } else if (averageScore >= 0 && averageScore <= 59) {
             return (`Rata-rata = ${averageScore} \nGrade = E`);
         } else {
-            return (`You are not very bright, are you?`);
+            return ('You are not very bright, are you?');
         }
     }
 }
 
-console.log(gradeCalculator(80, 84, 96, 77));
+console.log(gradeCalculator(80, 84, 96, 90));
--- printTriangle.js
+++ printTriangle.js
@@ -1,41 +1,41 @@
 //  * Case 3
 
 let printSegitiga = 5;
-let pattern = ``;
+let pattern = '';
 
 // validate input
-if (typeof printSegitiga === `number`) {
+if (typeof printSegitiga === 'number') {
     // outer loop will iterate vertically
     for (i = 1; i <= printSegitiga; i++) {
         // inner loop will iterate horizontally
         for (j = 1; j <= printSegitiga - i + 1; j++) {
           pattern += `${j} `;
         }
-        pattern += `\n`;
+        pattern += '\n';
     }
     console.log(pattern);
 } else {
-    console.log(`Please input number only!`);
+    console.log('Data must be a number!');
 }
 
 
 // * Case 3: Pattern in function
 
 printSegitiga = (num) => {
-    let pattern = ``;
+    let pattern = '';
 
-    if (typeof num === "number") {
+    if (typeof num === 'number') {
         for (i = 1; i <= num; i++) {
             for (j = 1; j <= num - i + 1; j++) {
                 pattern += `${j} ` ;
             }
-            pattern += `\n`;
+            pattern += '\n';
         }
     } else {
-        return `Data harus number!`;
+        return 'Data must be a number!';
     }
     
     return pattern;
 }
 
-console.log(printSegitiga(5)); 
+console.log(printSegitiga(8)); 
