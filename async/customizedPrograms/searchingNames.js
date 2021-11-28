41dbeaf24e178cd5a052e33feb8b2446c33c0d48
ferdif <ferdifh82@gmail.com>
2021-11-29 04:26:43 +0700

    add case 3 customize program 1

---
+++ async/customizedPrograms/searchingNames.js
@@ -0,0 +1,81 @@
+// * Case 3: Program 1 - Searching Names
+
+const people = [
+  "Zayn",
+  "Kurt",
+  "Guido van Rossum",
+  "Simon",
+  "Mirici",
+  "Layla",
+  "Lucas",
+  "Lucy",
+  "Ferdi",
+  "Michael",
+  "Abigail",
+  "Alexandra",
+  "Alison",
+  "Amanda",
+  "Angela",
+  "Bella",
+  "Carol",
+  "Caroline",
+  "Carolyn",
+  "Deidre",
+  "Diana",
+  "Elizabeth",
+  "Ella",
+  "Faith",
+  "Olivia",
+  "Penelope",
+];
+
+// main function
+const filterName = (keyword) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const filtrate = people.filter((value) =>
+        value.toLowerCase().includes(keyword.toLowerCase())
+      );
+      const sorting = filtrate.sort();
+      if (sorting.length !== 0) {
+        resolve(sorting);
+      } else {
+        reject(new Error("The List Is Empty"));
+      }
+    }, 3000);
+  });
+};
+
+// message
+console.log("Filtering Name...");
+
+// consuming promise
+const resolvedValue = (resolve) => {
+  console.log("Here is the list of name: ");
+  console.log(resolve);
+};
+const rejectedValue = (reject) => {
+  console.log(reject);
+  console.log(
+    "Keyword that you input is not match with the names in Database!"
+  );
+};
+
+filterName("di").then(resolvedValue).catch(rejectedValue);
+
+// handling error with try-catch
+const checkError = async () => {
+  try {
+    const filteredNames = await filterName("do");
+    console.log("Here is the list of name: ");
+    console.log(filteredNames);
+  } catch (error) {
+    console.log(error.stack);
+    console.log("The keyword is not match!");
+  } finally {
+    setTimeout(() => {
+      console.log("Do you want to try other keyword?");
+    }, 1000);
+  }
+};
+checkError();
