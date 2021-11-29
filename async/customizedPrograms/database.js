63bb9692e503689be0ee918e3b3e734e75aa890f
ferdif <ferdifh82@gmail.com>
2021-11-30 05:22:11 +0700

    add database prototype

---
+++ async/customizedPrograms/database.js
@@ -0,0 +1,63 @@
+// database prototype
+
+// data for Ice Cream Factory program
+const stocks = {
+  flavours: [
+    "Strawberry",
+    "Chocolate",
+    "Vanilla",
+    "Matcha",
+    "Salted Egg",
+    "Wasabi",
+    "Honey",
+    "Red Bean",
+    "Taro",
+    "Sweet Potato",
+    "Jasmine Flower",
+    "Squid Ink",
+  ],
+  toppings: [
+    "Crackers",
+    "Squid",
+    "Ketchup",
+    "Anchovies",
+    "Dried Seaweed",
+    "Rendang",
+    "Terasi",
+    "Takoyaki",
+    "Bacon",
+    "Scrumbled Eggs",
+  ],
+};
+
+// data for Searching Names program
+const people = [
+  "Zayn",
+  "Kurt",
+  "Guido van Rossum",
+  "Simon",
+  "Mirici",
+  "Layla",
+  "Lucas",
+  "Lucy",
+  "Ferdi",
+  "Michael",
+  "Abigail",
+  "Alexandra",
+  "Alison",
+  "Amanda",
+  "Angela",
+  "Bella",
+  "Carol",
+  "Caroline",
+  "Carolyn",
+  "Deidre",
+  "Diana",
+  "Elizabeth",
+  "Ella",
+  "Faith",
+  "Olivia",
+  "Penelope",
+];
+
+module.exports = { stocks, people };
